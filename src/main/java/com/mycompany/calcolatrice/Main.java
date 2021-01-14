08bd0d4a1767eba79867c5c4f184dc55d7f6ce59
Filippo Barucci <filibarucci@gmail.com>
2021-01-14 20:09:29 +0100

    Calcolatrice con somma e moltiplicazione

---
+++ src/main/java/com/mycompany/calcolatrice/Main.java
@@ -0,0 +1,46 @@
+package com.mycompany.calcolatrice;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        String expression;
+        Scanner scanner = new Scanner(System.in);
+        boolean var = false;
+        do {
+        double a = 0, b = 0, ris;
+        char op = ' ';
+        System.out.println("Inserire l'espressione che si desidera risolvere utilizzando questa \"sintassi\": num1 op num2");
+        expression = scanner.nextLine();
+        var = false;
+        try { stringManager(expression, a, b, op); } catch (InvalidUserInputException e) { System.out.println("L'input inserito non è corretto, usare la sintassi corretta"); var = true; }
+        } while(var);
+    }
+    
+    public static void stringManager(String string, double a, double b, char op) throws InvalidUserInputException {
+        String num1 = "", num2 = "", operator = "";
+        int index = 0;
+        
+        for (int i = index; i < string.length(); i++) {
+            if (string.charAt(i) >= 48 && string.charAt(i) < 58) num1 += string.charAt(i);
+            else if (string.charAt(i) == ' ') { index = i; break; }
+            else throw new InvalidUserInputException();
+        }
+        
+        for (int i = index; i < string.length(); i++) {
+            if (string.charAt(i) == ' ') { index = i; break; }
+            operator += string.charAt(i);
+        }
+        if (operator.length() > 1) throw new InvalidUserInputException();
+        
+        for (int i = index; i < string.length(); i++) {
+            if (string.charAt(i) >= 48 && string.charAt(i) < 58) num2 += string.charAt(i);
+            else if (string.charAt(i) == ' ') break;
+            else throw new InvalidUserInputException();
+        }   
+        
+        a = Double.parseDouble(num1);
+        b = Double.parseDouble(num2);
+        op = operator.charAt(0);
+    }
+}
