dd55e8115a50bd7d7ceb063954316fb78bd20d0c
Filippo Barucci <filibarucci@gmail.com>
2021-01-14 16:08:53 +0100

    Aggiunta della classe Calculator

---
+++ src/main/java/com/mycompany/calcolatrice/Calculator.java
@@ -0,0 +1,13 @@
+package com.mycompany.calcolatrice;
+
+public class Calculator {
+    double a, b;
+    char op;
+    
+    public Calculator(double a, double b, char op) { 
+        this.a = a;
+        this.b = b;
+        this.op = op;
+    }
+    
+}
